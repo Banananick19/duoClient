a947a3cca5230df3806a103b71ce71af7ff32679
banana <banananick19@mail.ru>
2021-08-08 12:45:13 +0300

    Круто(нет)

--- app/src/main/java/banana/duo/Client/ConnectActivity.java
+++ app/src/main/java/banana/duo/Client/ConnectActivity.java
@@ -7,9 +7,14 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.IOException;
+
+import banana.duo.Common.Message;
+import banana.duo.Common.MessageType;
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class ConnectActivity extends AppCompatActivity {
     private String ip;
@@ -33,6 +38,32 @@ public class ConnectActivity extends AppCompatActivity {
         System.out.println(ip + ":" + port);
         connectTask = new ConnectTask();
         connectTask.execute(ip, String.valueOf(port));
+        JoystickView joystick = (JoystickView) findViewById(R.id.joyStickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                System.out.println(angle + "-" + strength);
+                System.out.println(Math.cos(Math.toRadians(angle)) + "+" + Math.sin(Math.toRadians(angle)));
+                int x = (int) (Math.cos(Math.toRadians(angle)) * strength) / 10;
+                int y = - (int )  (Math.sin(Math.toRadians(angle)) * strength) / 10;
+                SendTask sendTask = new SendTask();
+                sendTask.execute(new Message(MessageType.MouseMove, x + "|" + y).toString());
+            }
+        });
+    }
+
+    class SendTask extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... strings) {
+            try {
+                client.sendMessage(Message.parseString(strings[0]));
+                System.out.println("Sended");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
     }
 
     class ConnectTask extends AsyncTask<String, Void, Boolean> {
--- app/src/main/java/banana/duo/Common/Message.java
+++ app/src/main/java/banana/duo/Common/Message.java
@@ -4,17 +4,19 @@ public class Message {
     private MessageType messageType;
     private String content;
 
-    private Message(MessageType messageType, String content) {
+    public Message(MessageType messageType, String content) {
         this.messageType = messageType;
         this.content = content;
     }
 
     public static Message parseString(String string) {
-        String stringMessageType = string.substring(string.indexOf("<") + 1);
-        stringMessageType = string.substring(0, string.indexOf(">"));
-        String stringContent = string.substring(string.indexOf(">") + 1);;
-        stringContent = string.substring(0, string.indexOf("\n"));
+        String stringMessageType = string.substring(string.indexOf("<") + 1, string.indexOf(">"));
+        String stringContent = string.substring(string.indexOf(">") + 1);
         return new Message(MessageType.valueOf(stringMessageType), stringContent);
+    }
+
+    public String toString() {
+        return "<" + messageType.toString() + ">" + content;
     }
 
 
